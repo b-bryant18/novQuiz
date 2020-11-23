ca967632b9d71f0d17295a0c23bcff252f9b837d
Brad Bryant <bradleybryant86@gmail.com>
2020-11-23 01:02:49 -0600

    Wrote lines for creating list elements for highscore anmes and score values

--- highscores.js
+++ highscores.js
@@ -2,8 +2,12 @@ const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 //Always JSON.parse when getting items from LS
 
+//Displays usernames and score values 
 highScoresList.innerHTML =
     highScores.map(score => {
         return `<li class="high-score>${score.name} - ${score.score}</li>`;
     })
         .join("");
+
+//A new li is created for each object in the highScores array
+//.map creates a new array by calling a specified function on each element in the parent array
