419bcf469d481ba756629ff93f295d499fccb8e0
Brad Bryant <bradleybryant86@gmail.com>
2020-11-25 01:17:47 -0600

    Changed spacing for legibility

--- game.js
+++ game.js
@@ -11,16 +11,32 @@ let availableQuestions = [];
 
 let questions = [];
 
-//Fetch questions from questions.json
+//Fetch questions from Open Trivia API
 fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
     .then(res => {
         return res.json();
     })
     .then(loadedQuestions => {
         console.log(loadedQuestions.results)
-        questions = loadedQuestions;
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+            //  Randomly splices correct answer into answer choices
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(formattedQuestion.answer - 1, 0,
+                loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
         startGame();
-    }).catch(err => {
+    })
+    .catch(err => {
         console.log(err);
     });
 
